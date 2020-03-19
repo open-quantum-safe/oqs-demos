8b3934bf61d36b2ef7dab5cf5f122bc41a286986
Michael Baentsch <57787676+baentsch@users.noreply.github.com>
2020-03-19 07:03:18 +0100

    addressing PR change requests

--- .circleci/git_no_checkin_in_last_day.sh
+++ .circleci/git_no_checkin_in_last_day.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+set -e 
+
 # Detects whether there has been a Git commit in the last day on this
 # branch. Returns 1 if there has been a commit, 0 if there has not.
 
-r=`git log --name-only --since="1 day ago" -n 2`
+r=$(git log --name-only --since="1 second ago" -n 2)
 if [ "x$r" == "x" ]; then 
     echo "No oqs-demos commit in the last day. Checking liboqs now."
     if [ -d tmp/liboqs ]; then
             cd tmp/liboqs
-            r=`git log --name-only --since="1 day ago" -n 2`
+            r=$(git log --name-only --since="1 day ago" -n 2)
             if [ "x$r" == "x" ]; then 
                 echo "Also no liboqs commit in the last day. Now checking openssl."
                 if [ -d ../tmp/openssl ]; then
             		cd ../tmp/openssl
-            		r=`git log --name-only --since="1 day ago" -n 2`
+            		r=$(git log --name-only --since="1 day ago" -n 2)
             		if [ "x$r" == "x" ]; then 
                 		echo "Also no openssl commit in the last day. Build not necessary."
                 		exit 0
