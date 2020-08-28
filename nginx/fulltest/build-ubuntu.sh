8661765df6f703bc317f1d3e11b3cdf762d459df
Michael Baentsch <57787676+baentsch@users.noreply.github.com>
2020-08-28 17:59:22 +0200

    version-enablement testserver (#50)

--- nginx/fulltest/build-ubuntu.sh
+++ nginx/fulltest/build-ubuntu.sh
@@ -4,6 +4,10 @@ export OPENSSL_PATH=/tmp/opt/openssl
 # define the nginx version to include
 export NGINX_VERSION=1.16.1
 
+# define the OQS releases to use, unset to deploy master branch
+export LIBOQS_RELEASE=0.4.0
+export OPENSSL_RELEASE=OQS-OpenSSL_1_1_1-stable-snapshot-2020-08
+
 # Define the degree of parallelism when building the image; leave the number away only if you know what you are doing
 export MAKE_DEFINES="-j 4"
 
@@ -13,8 +17,14 @@ sudo apt install libtool automake autoconf cmake make openssl git wget libssl-de
 
 # get OQS sources
 rm -rf /tmp/opt && mkdir /tmp/opt && cd /tmp/opt
+if [ -z "$LIBOQS_RELEASE" ]; then
 git clone --depth 1 --branch master https://github.com/open-quantum-safe/liboqs && \
-git clone --depth 1 --branch OQS-OpenSSL_1_1_1-stable https://github.com/open-quantum-safe/openssl && \
+git clone --depth 1 --branch OQS-OpenSSL_1_1_1-stable https://github.com/open-quantum-safe/openssl 
+else
+echo "Deploying stable liboqs release ${LIBOQS_RELEASE}"
+wget https://github.com/open-quantum-safe/liboqs/archive/${LIBOQS_RELEASE}.tar.gz && tar xzvf ${LIBOQS_RELEASE}.tar.gz && mv liboqs-${LIBOQS_RELEASE} liboqs && \
+wget https://github.com/open-quantum-safe/openssl/archive/${OPENSSL_RELEASE}.tar.gz && tar xzvf ${OPENSSL_RELEASE}.tar.gz && mv openssl-${OPENSSL_RELEASE} openssl 
+fi
 wget nginx.org/download/nginx-${NGINX_VERSION}.tar.gz && tar -zxvf nginx-${NGINX_VERSION}.tar.gz;
 
 # build liboqs (static only)
