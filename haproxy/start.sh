0848e832bf62d3a047af510385adce9fbc775cf7
Michael <57787676+baentsch@users.noreply.github.com>
2020-12-08 07:39:57 +0100

    haproxy added

---
+++ haproxy/start.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ "x$KEM_ALG" != "x" ]; then
+   # kem name given, set it
+   echo "Setting KEM alg $KEM_ALG"
+   sed -i "s/kyber768/$KEM_ALG/g" /opt/haproxy/conf/haproxy.cfg
+fi
+
+cd /opt/haproxy
+
+if [ $# -eq 1 ]; then
+   # backend address as sole optional parameter
+   echo "Setting target backend $1"
+   sed -i "s/127.0.0.1:8282/$1/g" /opt/haproxy/conf/haproxy.cfg 
+fi
+
+# Start backends:
+lighttpd -D -f /etc/lighttpd/lighttpd.conf &
+lighttpd -D -f /etc/lighttpd/lighttpd2.conf &
+
+sleep 2
+
+cat pki/server.crt pki/server.key > certkey.pem
+
+# Start HAProxy:
+/opt/oqssa/sbin/haproxy -f /opt/haproxy/conf/haproxy.cfg
+
