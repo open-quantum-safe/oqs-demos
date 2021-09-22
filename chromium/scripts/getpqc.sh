2c20b8d2a12bae11a9a1cccdcf3b992c1f919c21
Julien Pierre <julien.pierre@verizon.com>
2021-09-22 15:38:10 -0700

    Remove invocations of the time command.

--- chromium/scripts/getpqc.sh
+++ chromium/scripts/getpqc.sh
@@ -2,7 +2,7 @@
 mkdir -p $PROJECT
 cd $PROJECT
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-time fetch --nohooks chromium
+fetch --nohooks chromium
 cd src
 git checkout 94.0.4602.0
-time gclient sync -D
+gclient sync -D
