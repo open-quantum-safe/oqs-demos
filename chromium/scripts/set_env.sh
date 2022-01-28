9fd9230023f6c8988af9fb88dfe12c2d1c043c5c
xvzcf <xvzcf@users.noreply.github.com>
2022-01-28 11:30:43 -0500

    Updated chromium tag.

--- chromium/scripts/set_env.sh
+++ chromium/scripts/set_env.sh
@@ -3,7 +3,7 @@ export CHROMIUM_ROOT=$PROJECT/src
 export PATH=$PATH:$PROJECT/depot_tools
 if [ -z "$CHROMIUM_TAG" ]
 then
-export CHROMIUM_TAG=94.0.4602.0
+export CHROMIUM_TAG=100.0.4856.2
 fi
 if [ -z "$CHROMIUM_PATCH" ]
 then
