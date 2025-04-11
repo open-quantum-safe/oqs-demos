bb8a99253c39a7f5100f5d63f81366e4b88dcce4
Dave Kelsey <d_kelsey@uk.ibm.com>
2025-04-11 10:12:22 +0100

    add interop testing for nodejs demo
    Signed-off-by: Dave Kelsey <d_kelsey@uk.ibm.com>

--- nodejs/testserver.js
+++ nodejs/testserver.js
@@ -1,56 +1,31 @@
 const http2 = require('http2');
 const fs = require('fs');
-const { exit } = require('process');
 
 const options = {
   key: fs.readFileSync('./server_key.key'),
   cert: fs.readFileSync('./server_cert.crt'),
-  allowHTTP1: false,
+  allowHTTP1: true,
+  //could override the default openssl defined curves
   //ecdhCurve: 'X25519MLKEM768:mlkem1024:p521_mlkem1024:mlkem768:p384_mlkem768:mlkem512:p256_mlkem512:secp521r1:secp384r1:X25519:prime256v1:X448'
 };
 
 const port = 8443;
 
-// Create the HTTP/2 server
-const server = http2.createSecureServer(options);
+const server = http2.createSecureServer(options, (req, res) => {
+  if (req.url === '/hello') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World!\n');
+  } else if (req.url === '/exit') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Terminating Server\n');
+    setImmediate(() => {exit(0)});
 
-server.on('stream', (stream, headers) => {
-
-  // Check if the requested path is 'hello' or 'exit'
-  const path = headers[':path'];
-  console.log(path);
-
-  switch (path) {
-    case '/hello':
-      stream.respond({
-        ':status': 200,
-        'content-type': 'text/plain',
-      });
-      stream.write('Hello, World!');
-      stream.end();
-      break;
-
-    case '/exit':
-      // Close the connection when the path is /exit
-      stream.respond({
-        ':status': 200,
-        'content-type': 'text/plain',
-      });
-      stream.write('Terminating Server');
-      stream.end();
-      setImmediate(() => {exit(0)});
-      break;
-
-    default:
-      // Respond with a 404 Not Found for any other paths
-      stream.respond({
-        ':status': 404,
-        'content-type': 'text/plain'
-      });
-      stream.write('Not Found');
-      stream.end();
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
   }
 });
 
-server.listen(port);
-console.log('Server running at https://localhost:' + port);
+server.listen(port, () => {
+    console.log('Server running at https://localhost:' + port);
+});
